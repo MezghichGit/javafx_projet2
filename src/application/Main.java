870dc4a208ff35efa9605f67a732c620a4ea46fc
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2022-06-20 10:24:49 +0100

    css

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,7 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -10,22 +11,29 @@ import javafx.scene.layout.VBox;
 
 
 public class Main extends Application {
+	String nom;
 	@Override
 	public void start(Stage primaryStage) {
 		Button Button1 = new Button("Button1"); 
         Button Button2 = new Button("Button2"); 
         Button Button3 = new Button("Button3"); 
              
-        //VBox layout = new VBox(Button1, Button2, Button3);
-        VBox layout = new VBox();
+        
+        //VBox layout = new VBox();
         //layout.setAlignment(Pos.BASELINE_CENTER);
         //layout.setAlignment(Pos.BOTTOM_CENTER);
-        layout.setAlignment(Pos.CENTER);
-        layout.setSpacing(20);
-        layout.getChildren().addAll(Button1, Button2, Button3);
+        //layout.setAlignment(Pos.CENTER);
+        //layout.setSpacing(20);
+        VBox layout = new VBox(Button1, Button2, Button3);
+        
+        VBox.setMargin(Button1, new Insets(40,0,60,120)); // top right buttom left
+        VBox.setMargin(Button2, new Insets(40,0,0,120));
+        VBox.setMargin(Button3, new Insets(40,0,0,120));
+        
+        //layout.getChildren().addAll(Button1, Button2, Button3);
         
         Scene scene = new Scene(layout, 300, 300);  
- 
+        scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
         primaryStage.setTitle("Les Layouts en JavaFX");
         primaryStage.setScene(scene);   
         primaryStage.show(); 
